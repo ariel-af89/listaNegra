4ee9d8db0060af082ea64e18f1bb8148e684918e
Vic <Santana>
2020-01-23 11:50:27 -0600

    se actualiza proyecto
    se corrige encoding. ??

--- src/main/java/com/promad/msi/listaplacas/model/RespuestaModel.java
+++ src/main/java/com/promad/msi/listaplacas/model/RespuestaModel.java
@@ -1,6 +1,5 @@
 package com.promad.msi.listaplacas.model;
 
-import javax.validation.constraints.NotNull;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -12,7 +11,7 @@ public class RespuestaModel {
 	
 	@ApiModelProperty(position = 1, required = true,  example = "00", value = "El estatus de la respuesta", name="Estatus"  )
 	private String estatus;
-	@ApiModelProperty(position = 2, required = true,  example = "Registro en Lista Negra Satisfactorio", value = "Mensaje de operación",  name="Mensaje")
+	@ApiModelProperty(position = 2, required = true,  example = "Registro en Lista Negra Satisfactorio", value = "Mensaje de operaciÃ³n",  name="Mensaje")
 	private String mensaje;
 
 }
--- src/main/java/com/promad/msi/listaplacas/model/VehiculoInvolucradoModel.java
+++ src/main/java/com/promad/msi/listaplacas/model/VehiculoInvolucradoModel.java
@@ -19,31 +19,31 @@ public class VehiculoInvolucradoModel implements Serializable{
 	private static final long serialVersionUID = -7517834518371754134L;
 
 	@NotNull(message = "no puede ser nulo" )// 
-	@Pattern(regexp ="^((([A-Z0-9]{2,4}))-?(((([A-Z0-9]{2,4})-?)|(([A-Z0-9]{2}-[A-Z0-9]{2,3})))))$|(([0-9]{4}-[A-Z])$|(([0-9])-[A-Z]{2}-[0-9]{3}))$", message = "No es un formato de placa válido" )
-	@ApiModelProperty(position = 1, required = true,  example = "123ABC", value = "Placa del vehículo (sin caracteres extraños o espacios)", name="Placa")
+	@Pattern(regexp ="^((([A-Z0-9]{2,4}))-?(((([A-Z0-9]{2,4})-?)|(([A-Z0-9]{2}-[A-Z0-9]{2,3})))))$|(([0-9]{4}-[A-Z])$|(([0-9])-[A-Z]{2}-[0-9]{3}))$", message = "No es un formato de placa vÃ¡lido" )
+	@ApiModelProperty(position = 1, required = true,  example = "123ABC", value = "Placa del vehÃ­culo (sin caracteres extraï¿½os o espacios)", name="Placa")
 	private String placa;
 	
-	@ApiModelProperty(position = 2, required = false,  example = "10", value = "Correspondiente a la Marca del vehículo (Catálogo)", name="id Marca")
+	@ApiModelProperty(position = 2, required = false,  example = "10", value = "Correspondiente a la Marca del vehï¿½culo (CatÃ¡logo)", name="id Marca")
 	private int idMarca;
 	
-	@ApiModelProperty(position = 3, required = false,  example = "5", value = "Corresponde a la Submarca (Catálogo)", name="id SubMarca")
+	@ApiModelProperty(position = 3, required = false,  example = "5", value = "Corresponde a la Submarca (Catalogo)", name="id SubMarca")
 	private int idSubMarca;
 	
-	@ApiModelProperty(position = 4, required = false,  example = "2018", value = "Corresponderá al año del vehículo", name="modelo")
+	@ApiModelProperty(position = 4, required = false,  example = "2018", value = "CorresponderÃ¡ al aÃ±o del vehÃ­culo", name="modelo")
 	private String modelo;
 	
-	@ApiModelProperty(position = 5, required = false,  example = "20", value = "Corresponde al tipo de vehículo (Catálogo)", name="id Tipo")
+	@ApiModelProperty(position = 5, required = false,  example = "20", value = "Corresponde al tipo de vehÃ­culo (CatÃ¡logo)", name="id Tipo")
 	private int idTipo;
 	
-	@ApiModelProperty(position = 6, required = false,  example = "ROJO", value = "Color del vehículo", name="Color")
+	@ApiModelProperty(position = 6, required = false,  example = "ROJO", value = "Color del vehÃ­culo", name="Color")
 	private String color;
 	
-	@ApiModelProperty(position = 7, required = false,  example = "16", value = "Corresponde a la Ciudad de origen del vehículo (Catálogo)", name="id Procedencia")
+	@ApiModelProperty(position = 7, required = false,  example = "16", value = "Corresponde a la Ciudad de origen del vehï¿½culo (CatÃ¡logo)", name="id Procedencia")
 	private int idProcedencia;
 	
-	@ApiModelProperty(position = 8, required = false,  example = "5", value = "Origen del reporte (Catálogo)", name="Origen")
+	@ApiModelProperty(position = 8, required = false,  example = "5", value = "Origen del reporte (CatÃ¡logo)", name="Origen")
 	private int origen;
 	
-	@ApiModelProperty(position = 9, required = false,  example = "1", value = "Tipo de lista a la cual pertenecerá el vehículo* (Catálogo)", name="Tipo de Lista")
+	@ApiModelProperty(position = 9, required = false,  example = "1", value = "Tipo de lista a la cual pertenecerÃ¡ el vehï¿½culo* (CatÃ¡logo)", name="Tipo de Lista")
 	private int tipoLista;
 }

5912552f1d8e3abbe7648dda635641ac4a9a3601
Vic <Santana>
2020-01-28 17:18:50 -0600

    Se actualiza validación
    Se actualiza validación de matricula basado en la NORMA OFICIAL MEXICANA NOM-001-SCT-2-2016

--- src/main/java/com/promad/msi/listaplacas/model/VehiculoInvolucradoModel.java
+++ src/main/java/com/promad/msi/listaplacas/model/VehiculoInvolucradoModel.java
@@ -19,14 +19,14 @@ public class VehiculoInvolucradoModel implements Serializable{
 	private static final long serialVersionUID = -7517834518371754134L;
 
 	@NotNull(message = "no puede ser nulo" )// 
-	@Pattern(regexp ="^((([A-Z0-9]{2,4}))-?(((([A-Z0-9]{2,4})-?)|(([A-Z0-9]{2}-[A-Z0-9]{2,3})))))$|(([0-9]{4}-[A-Z])$|(([0-9])-[A-Z]{2}-[0-9]{3}))$", message = "No es un formato de placa válido" )
-	@ApiModelProperty(position = 1, required = true,  example = "123ABC", value = "Placa del vehículo (sin caracteres extra�os o espacios)", name="Placa")
+	@Pattern(regexp ="^(([A-Z0-9]{5,6})|(([A-Z0-9]{5}-([A-Z]{2}))|([A-Z0-9]{1,3}-[A-Z0-9]{2,5}))|((([A-Z0-9]{1}-[A-Z0-9]{2,5}-[A-Z0-9]{1,3})|([A-Z0-9]{2}-[A-Z0-9]{1,4}-[A-Z0-9]{1,3})|([A-Z0-9]{3}-[A-Z0-9]{2,3}-[A-Z0-9]{1,3}))))$", message = "No es un formato de placa válido" )
+	@ApiModelProperty(position = 1, required = true,  example = "123ABC", value = "Placa del vehículo (sin caracteres extraños o espacios)", name="Placa")
 	private String placa;
 	
-	@ApiModelProperty(position = 2, required = false,  example = "10", value = "Correspondiente a la Marca del veh�culo (Catálogo)", name="id Marca")
+	@ApiModelProperty(position = 2, required = false,  example = "10", value = "Correspondiente a la Marca del vehículo (Catálogo)", name="id Marca")
 	private int idMarca;
 	
-	@ApiModelProperty(position = 3, required = false,  example = "5", value = "Corresponde a la Submarca (Catalogo)", name="id SubMarca")
+	@ApiModelProperty(position = 3, required = false,  example = "5", value = "Corresponde a la Submarca (Catálogo)", name="id SubMarca")
 	private int idSubMarca;
 	
 	@ApiModelProperty(position = 4, required = false,  example = "2018", value = "Corresponderá al año del vehículo", name="modelo")
@@ -38,12 +38,12 @@ public class VehiculoInvolucradoModel implements Serializable{
 	@ApiModelProperty(position = 6, required = false,  example = "ROJO", value = "Color del vehículo", name="Color")
 	private String color;
 	
-	@ApiModelProperty(position = 7, required = false,  example = "16", value = "Corresponde a la Ciudad de origen del veh�culo (Catálogo)", name="id Procedencia")
+	@ApiModelProperty(position = 7, required = false,  example = "16", value = "Corresponde a la Ciudad de origen del vehículo (Catálogo)", name="id Procedencia")
 	private int idProcedencia;
 	
 	@ApiModelProperty(position = 8, required = false,  example = "5", value = "Origen del reporte (Catálogo)", name="Origen")
 	private int origen;
 	
-	@ApiModelProperty(position = 9, required = false,  example = "1", value = "Tipo de lista a la cual pertenecerá el veh�culo* (Catálogo)", name="Tipo de Lista")
+	@ApiModelProperty(position = 9, required = false,  example = "1", value = "Tipo de lista a la cual pertenecerá el vehículo* (Catálogo)", name="Tipo de Lista")
 	private int tipoLista;
 }

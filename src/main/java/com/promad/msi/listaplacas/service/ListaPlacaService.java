0c165db4738e63fb65eaca12e8a2204535094ebd
Vic <Santana>
2020-05-27 19:15:48 -0500

    se agrega el usuario para insertar en C5

--- src/main/java/com/promad/msi/listaplacas/service/ListaPlacaService.java
+++ src/main/java/com/promad/msi/listaplacas/service/ListaPlacaService.java
@@ -25,9 +25,13 @@ public class ListaPlacaService {
 	@Value("${url.save}")
 	private String save;
 	
+	@Value("${alta.usuario}")
+	private String usuarioAlta;
 	
 	public Object savePlaca(RegistroModel registroModel) {
 	
+		registroModel.getIncidenteModel().setUsuario(usuarioAlta);
+		
 		URI baseUrl = URI.create(save);
 		return listaPlacas.savePlaca(baseUrl,token,PlacasHelper.domainToRepository(registroModel));
 		
